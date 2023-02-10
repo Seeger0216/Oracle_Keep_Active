4794692c456d423e7c99d932c38f528cae680679
Seeger0216 <65768435+Seeger0216@users.noreply.github.com>
2023-02-10 19:59:55 +0800

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -2,17 +2,25 @@ import time
 import psutil
 import math
 import threading
+from multiprocessing import cpu_count
 
 target_cpu_utilization = 15
 Thread_num = 12
 starts = 1
-
+core = cpu_count()
+# 判断CPU核心数
+if core == 2:
+    sleep_sec = 0.6
+    
+if core == 4:
+    sleep_sec = 0.1
+    
 # 限制CPU在target_cpu_utilization以下
 def wait_for_cpu_utilization():
     while True:
         current_cpu_utilization = psutil.cpu_percent()
         if current_cpu_utilization >= target_cpu_utilization:
-            time.sleep(0.7)
+            time.sleep(sleep_sec)
         else:
             break
 
